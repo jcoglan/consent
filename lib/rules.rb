a942b3335f7f9827a9dcdf53d70ddda9bf62892e
James Coglan <jcoglan@googlemail.com>
2009-01-02 16:23:20 +0000

    Adding some docs for functionality present so far.

--- lib/rules.rb
+++ lib/rules.rb
@@ -4,10 +4,11 @@
 # 
 # In development mode, these rules are reloaded with every request, but
 # in production the rules are only loaded once and held in memory.
-# 
-# See vendor/plugins/consent/README for usage instructions.
 
 Consent.rules do
+  
+  # Add your access control rules here. See
+  # vendor/plugins/consent/README for examples.
   
 end
 
