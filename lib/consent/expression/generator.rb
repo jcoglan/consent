93024b4a186acb786ff42f00ec7cd41e9f9a85b7
James Coglan <jcoglan@googlemail.com>
2009-01-05 00:22:05 +0000

    When Expression::Generator is mixed in, remove all unnecessary instance methods so method_missing has maximum scope for being called.

--- lib/consent/expression/generator.rb
+++ lib/consent/expression/generator.rb
@@ -6,6 +6,17 @@ module Consent
         generate_expression(name, params, &block)
       end
       
+      def self.included(base)
+        meths = base.instance_methods -
+                Object.instance_methods.select { |m| m.to_s =~ /^__.*__$/ } -
+                base.instance_methods(false) -
+                instance_methods(false)
+        
+        meths = meths.map { |m| m.to_sym } - [:instance_eval]
+        
+        base.module_eval { undef_method(*meths) }
+      end
+      
     private
       
       def generate_expression(name, params, &block)
